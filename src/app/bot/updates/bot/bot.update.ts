4dffd99a956f0a82f73833c311139114e846653b
Lesha <lesha@MacBook-Pro-Lesha.local>
2023-03-01 02:19:31 +0200

    fiox

--- src/app/bot/updates/bot/bot.update.ts
+++ src/app/bot/updates/bot/bot.update.ts
@@ -18,6 +18,8 @@ export class BotUpdate {
 			await context.replyWithSticker(STICKERS.hello);
 		}
 
+		console.log('STARTED!');
+
 		await context.reply(text, {
 			reply_markup: {
 				inline_keyboard: [[{ text: "Почати", web_app: { url: `${environment.appUrl}?from=telegram` } }]]
